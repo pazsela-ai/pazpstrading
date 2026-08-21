81d44e0bca3e9d267019ed0dc6a9567c322a6e41
pazsela-ai <pazsela@gmail.com>
2026-08-21 09:39:43 +0300

    Update code6.py

--- code6.py
+++ code6.py
@@ -1,3 +1,22 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+
+keep_alive()
 import asyncio
 import logging
 import html
